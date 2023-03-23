f23c6e5eba2e09c7c18b3d22d539b12c4e7fb3e2
kwonkangbin <kbk282655@gmail.com>
2023-03-24 00:48:08 +0900

    Create : 헤더

---
+++ src/Components/Header.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import logo from "../Assets/Img/logo.png";
+
+export default function Header(){
+    return(
+        <Container>
+            <div>
+                <img src={logo} alt="Logo"/>
+                <div class="logo-text">
+                    Galphy
+                </div>
+            </div>
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    width: 100vw;
+	height: 71px;
+	background-color: white;
+    flex-grow: 0;
+    display: flex;
+    align-items: center;
+
+    > div{
+        display: flex;
+        flex-direction: row;
+
+        img{
+            margin: 0 0 0 160px;
+            width: 39px;
+            height: 39px;
+            object-fit: cover;
+        }
+        
+        .logo-text {
+            margin: 2px 0 0 10px;
+            width: fit-content;
+            height: fit-content;
+            font-family: Pretendard;
+            font-size: 30px;
+            font-weight: 800;
+            color: #866447;
+        }
+    }
+`;
