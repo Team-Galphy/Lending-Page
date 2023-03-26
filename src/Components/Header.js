e5ce793451a5c772b80f08b561815e4ed7125599
kwonkangbin <kbk282655@gmail.com>
2023-03-27 01:09:18 +0900

    UPDATE : Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,11 @@
 import styled from "styled-components";
-import logo from "../Assets/Img/logo.png";
+import logo from "../Assets/Img/main_logo.png";
 
 export default function Header(){
     return(
         <Container>
             <div>
                 <img src={logo} alt="Logo"/>
-                <div class="logo-text">
-                    Galphy
-                </div>
             </div>
         </Container>
     );
@@ -22,25 +19,10 @@ const Container = styled.header`
     display: flex;
     align-items: center;
 
-    > div{
-        display: flex;
-        flex-direction: row;
-
-        img{
-            margin: 0 0 0 160px;
-            width: 35px;
-            height: 35px;
-            object-fit: cover;
-        }
-        
-        .logo-text {
-            margin: 2px 0 0 10px;
-            width: fit-content;
-            height: fit-content;
-            font-family: Pretendard;
-            font-size: 27px;
-            font-weight: 800;
-            color: #866447;
-        }
+    img{
+        margin: 0 0 0 160px;
+        width: fit-content;
+        height: 35px;
+        object-fit: cover;
     }
 `;
