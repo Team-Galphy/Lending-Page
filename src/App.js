1812db87dc1569a14deb266b9802b53a42d0dbc8
kwonkangbin <kbk282655@gmail.com>
2023-03-27 01:13:01 +0900

    CREATE : Style.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import Header from "./Components/Header";
 import Main from "./Components/Main";
 import Footer from "./Components/Footer";
 import Card from "./Components/Card";
+import Home from "./Components/Home";
+import Checklist from "./Components/Checklist";
+import Library from "./Components/Library";
+import Profile from "./Components/Profile";
+import Rserve from "./Components/Reserve";
 
 function App() {
   return (
@@ -9,6 +14,11 @@ function App() {
       <Header />
       <Main />
       <Card />
+      <Home />
+      <Checklist />
+      <Library />
+      <Profile />
+      <Rserve />
       <Footer />
     </>
   );
---
+++ src/Style.js
@@ -0,0 +1,97 @@
+import styled from "styled-components";
+
+export const Container1 = styled.div`
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    > div{
+        display: flex;
+        flex-direction: row;
+        
+        > div{
+            margin-right: 220px;
+            
+            > div{
+                margin-top: 35px;
+
+                font-family: 'Pretendard';
+                font-style: normal;
+                font-weight: 600;
+                font-size: 25px;
+                line-height: 30px;
+
+                color: #3F3F3D;
+            }
+        }
+    }
+    img{
+        margin: 80px 0 0 0;
+        width: 210px;
+        height: auto;
+    }
+    h2{
+        font-family: 'Pretendard';
+        font-weight: 800;
+        font-size: 40px;
+        line-height: 48px;
+        color: #8C6141;
+        margin: 80px 0 30px 1px;
+    }
+    h3{
+        font-family: 'Pretendard';
+        font-style: normal;
+        font-weight: 800;
+        font-size: 70px;
+        line-height: 84px;
+
+        color: #B2B5B8;
+    }
+`;
+
+export const Container2 = styled.div`
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    > div{
+        display: flex;
+        flex-direction: row;
+        
+        > div{
+            margin-left: 220px;
+            
+            > div{
+                margin-top: 35px;
+
+                font-family: 'Pretendard';
+                font-style: normal;
+                font-weight: 600;
+                font-size: 25px;
+                line-height: 30px;
+
+                color: #3F3F3D;
+            }
+        }
+    }
+    img{
+        margin: 80px 0 0 0;
+        width: 210px;
+        height: auto;
+    }
+    h2{
+        font-family: 'Pretendard';
+        font-weight: 800;
+        font-size: 40px;
+        line-height: 48px;
+        color: #8C6141;
+        margin: 80px 0 30px 1px;
+    }
+    h3{
+        font-family: 'Pretendard';
+        font-style: normal;
+        font-weight: 800;
+        font-size: 70px;
+        line-height: 84px;
+
+        color: #B2B5B8;
+    }
+`;
