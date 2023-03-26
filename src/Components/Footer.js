00512f0ac3e1cb0c860f5070a824e4d0c3a2185f
kwonkangbin <kbk282655@gmail.com>
2023-03-26 12:07:05 +0900

    CREATE : Footer.js

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export default function Footer(){
+    return(
+        <Container>
+
+        </Container>
+    );
+}
+
+const Container = styled.footer`
+    width: 100vw;
+    height: 130px;
+    background-color: #DCDCDC;
+    bottom: 0;
+`;
