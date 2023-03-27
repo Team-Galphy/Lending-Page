c51ed2de011df3518398611c291684c2de885fea
kwonkangbin <kbk282655@gmail.com>
2023-03-27 23:10:52 +0900

    CREATE : Review.js

---
+++ src/Components/Common/Review.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export default function Review(){
+    return(
+        <Sdiv></Sdiv>
+    );
+}
+
+const Sdiv = styled.div`
+    width: 330px;
+    min-width: 330px;
+    height: 100%;
+    margin-right: 40px;
+
+    background: #D9D9D9;
+    border-radius: 35px;
+`;
