803bb989ca443ebbf68fa39fcbaa9bb31e4dc172
kwonkangbin <kbk282655@gmail.com>
2023-03-27 23:13:59 +0900

    CREATE : Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,15 +1,19 @@
 import styled from "styled-components";
+import Card1 from "./Cardarea/Card1";
+import Card2 from "./Cardarea/Card2";
+
 
 export default function Card(){
     return(
         <Container>
-
+            <Card1 />
+            <Card2 />
         </Container>
     );
 }
 
 const Container = styled.main`
+    margin-top: 40px;
     width: 100vw;
-    height: 440px;
-    background-color: red;
+    height: 360px;
 `;
