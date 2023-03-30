81b663bfdd4a030d302975ae53fe7c52a0ba2439
kwonkangbin <kbk282655@gmail.com>
2023-03-30 22:06:45 +0900

    UPDATE : Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,60 +5,88 @@ export default function Footer(){
     return(
         <Container>
             <div>
-                <img src={footer_logo} alt="푸터 로고"/>
-                <small>Copyright 2023. Galphy all rights reserved.</small>    
-            </div>
-            <div>
-                <a>개인정보처리방침</a>
-                <a>이용약관</a>
-                <div>000000000@galphy.com</div>
+                <S1>
+                    <img src={footer_logo} alt="푸터 로고"/>
+                    <small>Copyright 2023. Galphy all rights reserved.</small>    
+                </S1>
+                <div></div>
+                <S2>
+                    <div>
+                        <a>개인정보처리방침</a>
+                        <a>이용약관</a>
+                    </div>
+                    <div className="e-mail">000000000@galphy.com</div>
+                </S2>
             </div>
         </Container>
     );
 }
 
 const Container = styled.footer`
+    display: flex;
+    flex-direction: row;
+
     width: 100vw;
     height: 130px;
     background-color: #DCDCDC;
     bottom: 0;
-    
-    &:first-child{
-        > small{
-            font-family: 'Pretendard';
-            font-style: normal;
-            font-weight: 500;
-            font-size: 15px;
-            line-height: 18px;
 
-            color: #737373;
-        }
-    }
-
-    &:last-child{
-        > a {
-            font-family: 'Pretendard';
-            font-style: normal;
-            font-weight: 500;
-            font-size: 15px;
-            line-height: 18px;
-            /* identical to box height */
-
-            text-align: right;
-
-            color: #737373;
-        }
+    > div{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        padding: 0px 200px 0px 200px;
+        
         > div{
+            width: 100%;
+        }
+    }
+`;
+
+const S1 = styled.div`
+        margin-top: 24px;
+        display: flex;
+        flex-direction: column;
+
+
+        > img{
+            width: 150px;            
+        }
+        
+        > small{
+            margin-top: 20px;
             font-family: 'Pretendard';
             font-style: normal;
             font-weight: 500;
             font-size: 15px;
             line-height: 18px;
-            /* identical to box height */
-
-            text-align: right;
 
             color: #737373;
-        }
     }
+`;
+
+const S2 = styled.div`
+        margin-top: 46px;
+        display: flex;
+        flex-direction: column;
+
+
+        div{
+            font-family: 'Pretendard';
+            font-weight: 500;
+            font-size: 15px;
+            color: #737373;
+
+            text-align: end;
+
+            > a{
+                margin-left: 10px;
+            }
+        }
+
+
+        .e-mail{
+           margin-top: 25px;
+        }
 `;
