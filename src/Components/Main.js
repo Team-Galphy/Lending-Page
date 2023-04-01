fc609f8be8e0c5830356d938d3213ec8445e5969
kwonkangbin <kbk282655@gmail.com>
2023-04-01 23:07:25 +0900

    UPDATE : Style.js 넓이

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,79 +1,78 @@
 import styled from "styled-components";
 
-export default function Main(){
-    return(
-        <Container>
-            <h1 class="main-title">
-                우리가 원하던<br />
-                <span class="text-style-1">독서 플랫폼</span>
-            </h1>
-            <button>사전예약하기</button>
-        </Container>
-    );
+export default function Main() {
+  return (
+    <Container>
+      <h1 class="main-title">
+        우리가 원하던
+        <br />
+        <span class="text-style-1">독서 플랫폼</span>
+      </h1>
+      <button>사전예약하기</button>
+    </Container>
+  );
 }
 
 const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  .main-title {
+    margin-top: 155px;
+    font-family: "Pretendard";
+    font-size: 65px;
+    font-weight: 800;
+    text-align: center;
+    color: #a89684;
+
+    @media (max-width: 800px) {
+      font-size: 75px;
+    }
+
+    @media (max-width: 400px) {
+      font-size: 64px;
+    }
+
+    > br {
+      display: none;
+      @media (max-width: 800px) {
+        display: block;
+      }
+    }
+  }
+
+  .main-title .text-style-1 {
+    padding-left: 14px;
+    font-weight: 900;
+    color: #9d7c60;
+
+    @media (max-width: 800px) {
+      margin-top: 5px;
+      padding-left: 0px;
+    }
+  }
+
+  button {
     display: flex;
-    flex-direction: column;
-    .main-title {
-        margin-top: 155px;
-        font-family: 'Pretendard';
-        font-size: 65px;
-        font-weight: 800;
-        text-align: center;
-        color: #a89684;
+    justify-content: center;
+    align-items: center;
 
-        @media (max-width: 800px){
-            font-size: 75px;
-        }
+    width: 230px;
+    height: 66px;
+    margin: 0 auto;
+    margin-top: 90px;
+    border: 0px;
+    background: #9d7c60;
+    border-radius: 30px;
 
-        @media (max-width: 380px){
-            font-size: 60px;
-        }
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 30px;
+    line-height: 42px;
+    /* identical to box height */
 
-        > br{
-            display: none;
-            @media (max-width: 800px){
-                display: block;
-            }
-        }
-    }
+    text-align: center;
 
-    .main-title .text-style-1 {
-        padding-left: 14px;
-        font-weight: 900;
-        color: #9D7C60;
-
-        @media (max-width: 800px){
-            margin-top: 5px;
-            padding-left: 0px;
-        }
-    }
-
-    button{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        width: 230px;
-        height: 66px;
-        margin: 0 auto;
-        margin-top: 90px;
-        border: 0px;
-        background: #9D7C60;
-        border-radius: 30px;
-
-        font-family: 'Pretendard';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 30px;
-        line-height: 42px;
-        /* identical to box height */
-
-        text-align: center; 
-
-        color: #FFFFFF;
-    }
-
-
+    color: #ffffff;
+  }
 `;
--- src/Style.js
+++ src/Style.js
@@ -1,156 +1,158 @@
 import styled from "styled-components";
 
 export const Container1 = styled.div`
-    width: 100vw;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  overflow-x: hidden;
+  margin-top: 100px;
+
+  > div {
     display: flex;
-    justify-content: center;
-    overflow-x: hidden;
+    flex-direction: row;
 
-    > div{
-        display: flex;
-        flex-direction: row;
-
-        @media (max-width: 1000px){
-            flex-direction: column;
-        }
-        
-        > div{
-            margin-right: 250px;
-            @media (max-width: 1000px){
-                margin-right: 0px;
-            }
-            
-            > div{
-                margin-top: 35px;
-
-                font-family: 'Pretendard';
-                font-style: normal;
-                font-weight: 600;
-                font-size: 25px;
-                line-height: 30px;
-
-                color: #3F3F3D;
-
-                @media (max-width: 580px){
-                    font-size: 20px;
-                }
-            }
-        }
+    @media (max-width: 1000px) {
+      flex-direction: column;
     }
-    img{
-        margin: 80px 0 0 0;
-        width: 265px;
-        min-width: 60px;
-        height: auto;
 
-        @media (max-width: 620px){
-            margin: 40px 0 0 0;
-            width: 180px;
-        }
-    }
-    h2{
-        font-family: 'Pretendard';
-        font-weight: 800;
-        font-size: 40px;
-        line-height: 48px;
-        color: #8C6141;
-        margin: 80px 0 30px 1px;
+    > div {
+      margin-right: 250px;
+      @media (max-width: 1000px) {
+        margin-right: 0px;
+      }
 
-        @media (max-width: 580px){
-            font-size: 30px;
-            line-height: 34px;
-        }
-    }
-    h3{
-        font-family: 'Pretendard';
+      > div {
+        margin-top: 35px;
+
+        font-family: "Pretendard";
         font-style: normal;
-        font-weight: 800;
-        font-size: 70px;
-        line-height: 84px;
+        font-weight: 600;
+        font-size: 25px;
+        line-height: 30px;
 
-        color: #B2B5B8;
+        color: #3f3f3d;
 
-        @media (max-width: 580px){
-            font-size: 50px;
-            line-height: 55px;
+        @media (max-width: 580px) {
+          font-size: 20px;
         }
+      }
     }
+  }
+  img {
+    margin: 80px 0 0 0;
+    width: 285px;
+    min-width: 60px;
+    height: auto;
+
+    @media (max-width: 620px) {
+      margin: 40px 0 0 0;
+      width: 180px;
+    }
+  }
+  h2 {
+    font-family: "Pretendard";
+    font-weight: 800;
+    font-size: 40px;
+    line-height: 48px;
+    color: #8c6141;
+    margin: 80px 0 30px 1px;
+
+    @media (max-width: 580px) {
+      font-size: 30px;
+      line-height: 34px;
+    }
+  }
+  h3 {
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 800;
+    font-size: 70px;
+    line-height: 84px;
+
+    color: #b2b5b8;
+
+    @media (max-width: 580px) {
+      font-size: 50px;
+      line-height: 55px;
+    }
+  }
 `;
 
 export const Container2 = styled.div`
-    width: 100vw;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  overflow-x: hidden;
+  margin-top: 100px;
+  > div {
     display: flex;
-    justify-content: center;
-    overflow-x: hidden;
-    > div{
-        display: flex;
-        flex-direction: row;
-        
-        @media (max-width: 1000px){
-            flex-direction: column-reverse;
-        }
+    flex-direction: row;
 
-        > div{
-            margin-left: 250px;
-            
-            @media (max-width: 1000px){
-                margin-left: 0px;
-            }
-
-            > div{
-                margin-top: 35px;
-
-                font-family: 'Pretendard';
-                font-style: normal;
-                font-weight: 600;
-                font-size: 25px;
-                line-height: 30px;
-
-                color: #3F3F3D;
-                
-                @media (max-width: 580px){
-                    font-size: 20px;
-                }
-            }
-        }
-    }
-    img{
-        margin: 80px 0 0 0;
-        width: 265px;
-        min-width: 60px;
-        height: auto;
-
-        @media (max-width: 620px){
-            margin: 40px 0 0 0;
-            width: 180px;
-        }
+    @media (max-width: 1000px) {
+      flex-direction: column-reverse;
     }
 
-    h2{
-        font-family: 'Pretendard';
-        font-weight: 800;
-        font-size: 40px;
-        line-height: 48px;
-        color: #8C6141;
-        margin: 80px 0 30px 1px;
+    > div {
+      margin-left: 250px;
 
-        @media (max-width: 580px){
-            font-size: 30px;
-            line-height: 34px;
-        }
-    }
-    h3{
-        font-family: 'Pretendard';
+      @media (max-width: 1000px) {
+        margin-left: 0px;
+      }
+
+      > div {
+        margin-top: 35px;
+
+        font-family: "Pretendard";
         font-style: normal;
-        font-weight: 800;
-        font-size: 70px;
-        line-height: 84px;
+        font-weight: 600;
+        font-size: 25px;
+        line-height: 30px;
 
-        color: #B2B5B8;
+        color: #3f3f3d;
 
-        @media (max-width: 580px){
-            font-size: 50px;
-            line-height: 55px;
+        @media (max-width: 580px) {
+          font-size: 20px;
         }
+      }
     }
+  }
+  img {
+    margin: 80px 0 0 0;
+    width: 285px;
+    min-width: 60px;
+    height: auto;
+
+    @media (max-width: 620px) {
+      margin: 40px 0 0 0;
+      width: 180px;
+    }
+  }
+
+  h2 {
+    font-family: "Pretendard";
+    font-weight: 800;
+    font-size: 40px;
+    line-height: 48px;
+    color: #8c6141;
+    margin: 80px 0 30px 1px;
+
+    @media (max-width: 580px) {
+      font-size: 30px;
+      line-height: 34px;
+    }
+  }
+  h3 {
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 800;
+    font-size: 70px;
+    line-height: 84px;
+
+    color: #b2b5b8;
+
+    @media (max-width: 580px) {
+      font-size: 50px;
+      line-height: 55px;
+    }
+  }
 `;
