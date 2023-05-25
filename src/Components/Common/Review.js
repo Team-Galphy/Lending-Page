4508e96127bba6c4777b7c196cb8ebe9dc8295f8
kwonkangbin <kbk282655@gmail.com>
2023-05-26 01:33:48 +0900

    ADD : review.js

--- src/Components/Common/Review.js
+++ src/Components/Common/Review.js
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 
-export default function Review(){
+
+
+export default function Review({title, one, two}){
     return(
-        <Sdiv></Sdiv>
+        <Sdiv>
+            <Sh>{ title }</Sh>
+            <SP>{ one }<br></br>{ two }</SP>
+        </Sdiv>
     );
 }
 
@@ -14,4 +19,41 @@ const Sdiv = styled.div`
 
     background: #D9D9D9;
     border-radius: 35px;
+    position: relative;
+`;
+
+const Sh = styled.h4`
+    position: absolute;
+    width: fit-content;
+    height: 30px;
+    left: 24px;
+    top: 30px;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 22px;
+    line-height: 30px;
+
+    letter-spacing: -0.04em;
+
+    color: #000000;
+`;
+
+const SP = styled.p`
+    position: absolute;
+    width: fit-content;
+    height: 48px;
+    left: 24px;
+    top: 70px;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 15px;
+    line-height: 24px;
+
+    letter-spacing: -0.02em;
+
+    color: #4A4A4A;
 `;
