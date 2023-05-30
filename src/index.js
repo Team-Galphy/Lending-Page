d0ed0abc1788f35887937d2f81cfea48e176aa07
kwonkangbin <kbk282655@gmail.com>
2023-05-31 00:33:12 +0900

    흰바탕 에러 고침

--- src/index.js
+++ src/index.js
@@ -3,19 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './style/GlobalStyle';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
-      <GlobalStyle />
-      <Routes>
-          <Route path="/" element={<App />} />
-      </Routes>
-    </Router>
+      <>
+        <GlobalStyle />
+        <App />
+      </>
 );
 
