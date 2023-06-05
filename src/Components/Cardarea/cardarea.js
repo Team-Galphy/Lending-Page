27b13fdbfc5be0f708e21e8858827fd9b560cd43
kwonkangbin <kbk282655@gmail.com>
2023-06-05 22:41:49 +0900

    애니메이션 자연스럽게

--- src/Components/Cardarea/cardarea.js
+++ src/Components/Cardarea/cardarea.js
@@ -10,9 +10,6 @@ export default function Card(){
                     <Review title = '리뷰가 다 모여있어요' one='책 고를 때 사람들 의견은 여기 다 있으니까' two='이곳저곳 둘러볼 필요 없어서 엄청 편해요!'/>
                     <Review title = '인사이트가 풍부해요' one = '일반 유저뿐만 아니라 전문가가 쓴 글도 많아서' two = '배울 것과 얻어갈 것이 많아요.'/>
                     <Review title = '배울 것과 얻어갈 것이 많아요.' one='갈피의 체크리스트 글쓰기 시스템은' two = '저같은 사람도 글잘러로 만들어줘요.'/>
-                    <Review title = '무슨 책 읽지? 확인해봐요' one = '다른 유저들의 프로필을 클릭하고' two='독서 패턴과 취향을 확인해보세요.'/>
-                    <Review title='읽은 책을 정리할 수 있어요' one ='서재를 주제별·섹션별로 구분할 수 있어요.' two = '내가 원하는대로 커스텀도 가능하죠.'/>
-                    <Review title = '서로 소통하고 공유해요' one = '책을 읽고 느낀 감정, 짧은 생각들,' two='이런 저런 이야기들을 쉽고 가볍게 공유해요.'/>
                     <Review title = '매일 큐레이션받아요' one = '내 취향에 맞는 북 컨텐츠를 큐레이션해줘요.' two = '매일 홈에서 확인할 수 있어요.'/>
                     <Review title = '리뷰가 다 모여있어요' one='책 고를 때 사람들 의견은 여기 다 있으니까' two='이곳저곳 둘러볼 필요 없어서 엄청 편해요!'/>
                     <Review title = '인사이트가 풍부해요' one = '일반 유저뿐만 아니라 전문가가 쓴 글도 많아서' two = '배울 것과 얻어갈 것이 많아요.'/>
@@ -28,6 +25,9 @@ export default function Card(){
                     <Review title='읽은 책을 정리할 수 있어요' one ='서재를 주제별·섹션별로 구분할 수 있어요.' two = '내가 원하는대로 커스텀도 가능하죠.'/>
                     <Review title = '서로 소통하고 공유해요' one = '책을 읽고 느낀 감정, 짧은 생각들,' two='이런 저런 이야기들을 쉽고 가볍게 공유해요.'/>
                     <Review title = '매일 큐레이션받아요' one = '내 취향에 맞는 북 컨텐츠를 큐레이션해줘요.' two = '매일 홈에서 확인할 수 있어요.'/>
+                    <Review title = '인사이트가 풍부해요' one = '일반 유저뿐만 아니라 전문가가 쓴 글도 많아서' two = '배울 것과 얻어갈 것이 많아요.'/>
+                    <Review title = '배울 것과 얻어갈 것이 많아요.' one='갈피의 체크리스트 글쓰기 시스템은' two = '저같은 사람도 글잘러로 만들어줘요.'/>
+                    <Review title = '무슨 책 읽지? 확인해봐요' one = '다른 유저들의 프로필을 클릭하고' two='독서 패턴과 취향을 확인해보세요.'/>
                 </div>
                 <div className="bottom-cardarea">
                     <Review title='읽은 책을 정리할 수 있어요' one ='서재를 주제별·섹션별로 구분할 수 있어요.' two = '내가 원하는대로 커스텀도 가능하죠.'/>
@@ -36,9 +36,6 @@ export default function Card(){
                     <Review title = '리뷰가 다 모여있어요' one='책 고를 때 사람들 의견은 여기 다 있으니까' two='이곳저곳 둘러볼 필요 없어서 엄청 편해요!'/>
                     <Review title = '인사이트가 풍부해요' one = '일반 유저뿐만 아니라 전문가가 쓴 글도 많아서' two = '배울 것과 얻어갈 것이 많아요.'/>
                     <Review title = '배울 것과 얻어갈 것이 많아요.' one='갈피의 체크리스트 글쓰기 시스템은' two = '저같은 사람도 글잘러로 만들어줘요.'/>
-                    <Review title = '무슨 책 읽지? 확인해봐요' one = '다른 유저들의 프로필을 클릭하고' two='독서 패턴과 취향을 확인해보세요.'/>                   
-                    <Review title = '배울 것과 얻어갈 것이 많아요.' one='갈피의 체크리스트 글쓰기 시스템은' two = '저같은 사람도 글잘러로 만들어줘요.'/>
-                    <Review title = '무슨 책 읽지? 확인해봐요' one = '다른 유저들의 프로필을 클릭하고' two='독서 패턴과 취향을 확인해보세요.'/>
                     <Review title = '리뷰가 다 모여있어요' one='책 고를 때 사람들 의견은 여기 다 있으니까' two='이곳저곳 둘러볼 필요 없어서 엄청 편해요!'/>
                     <Review title = '인사이트가 풍부해요' one = '일반 유저뿐만 아니라 전문가가 쓴 글도 많아서' two = '배울 것과 얻어갈 것이 많아요.'/>
                     <Review title='읽은 책을 정리할 수 있어요' one ='서재를 주제별·섹션별로 구분할 수 있어요.' two = '내가 원하는대로 커스텀도 가능하죠.'/>
@@ -51,6 +48,9 @@ export default function Card(){
                     <Review title='읽은 책을 정리할 수 있어요' one ='서재를 주제별·섹션별로 구분할 수 있어요.' two = '내가 원하는대로 커스텀도 가능하죠.'/>
                     <Review title = '서로 소통하고 공유해요' one = '책을 읽고 느낀 감정, 짧은 생각들,' two='이런 저런 이야기들을 쉽고 가볍게 공유해요.'/>
                     <Review title = '매일 큐레이션받아요' one = '내 취향에 맞는 북 컨텐츠를 큐레이션해줘요.' two = '매일 홈에서 확인할 수 있어요.'/>
+                    <Review title = '인사이트가 풍부해요' one = '일반 유저뿐만 아니라 전문가가 쓴 글도 많아서' two = '배울 것과 얻어갈 것이 많아요.'/>
+                    <Review title = '배울 것과 얻어갈 것이 많아요.' one='갈피의 체크리스트 글쓰기 시스템은' two = '저같은 사람도 글잘러로 만들어줘요.'/>
+                    <Review title = '무슨 책 읽지? 확인해봐요' one = '다른 유저들의 프로필을 클릭하고' two='독서 패턴과 취향을 확인해보세요.'/>
                 </div>
             </div>
         </Container>
@@ -68,7 +68,10 @@ const Container = styled.main`
         flex-direction: column;
         width: 100%;
         height: 100%;
-        animation: marquee1 20s linear infinite;
+        animation: marquee1 40s linear infinite;
+        @media (max-width: 450px) {
+            animation: marquee1 25s linear infinite;
+        }
 
         > div{
             margin-top: 20px;
@@ -85,7 +88,7 @@ const Container = styled.main`
 
     @keyframes marquee1 {
         from {
-            transform: translateX(-50%);
+            transform: translateX(-400%);
         }
 
         to {
